b9d508c48cb4ec443861edd46162e110bff23f2f
Stuart Bowman <stuartsoft2012@gmail.com>
2015-09-06 00:29:51 -0400

    Adjusted the CubeMatrix input algorithm. It will now constrain the input and added a dampening effect aswell for a much smoother experience over the old raw-value input we had before

--- Assets/CubeMatrix.cs
+++ Assets/CubeMatrix.cs
@@ -7,9 +7,15 @@ public class CubeMatrix : MonoBehaviour {
 	public GameObject cubePrefab;
 	public float Multiplier = 200;
 	public int bins = 25;
+	public bool colorsEnabled = true;
+	float binMaxHeight = 20.0f;
+	float inputMultiplier = 0.15f;
+	float dampening = 0.9f;
+	float binSpacing = 2.0f;
 	public AudioSource AS;
 
 	List<List<GameObject>> matrix;
+	HSBColor hsbColor;
 	
 	// Use this for initialization
 	void Start () {
@@ -21,7 +27,7 @@ public class CubeMatrix : MonoBehaviour {
 		for (int i=0; i<bins*2; i++) {
 			List<GameObject> row = new List<GameObject>();
 			for (int j=0;j<bins*2;j++){
-				GameObject temp = Instantiate(cubePrefab, new Vector3(i*2+0.5f-bins*2,0.0f,j*2 + 0.5f - bins*2),Quaternion.identity) as GameObject;
+				GameObject temp = Instantiate(cubePrefab, new Vector3(i*binSpacing+0.5f-bins*binSpacing,0.0f,j*binSpacing + 0.5f - bins*binSpacing),Quaternion.identity) as GameObject;
 				//temp.transform.parent = transform;
 				row.Add(temp);
 			}
@@ -37,33 +43,61 @@ public class CubeMatrix : MonoBehaviour {
 		int i = 0;
 		
 		int lastBinIndex = (bins * 2) - 1;
+		if (!colorsEnabled)
+			hsbColor = new HSBColor(Color.white);
 		
 		while (i<bins){
 			float value = spectrum[i] * Multiplier;
 			for (int j=0;j<bins*2;j++){
 				
 				Vector3 scale = matrix[i][j].transform.localScale;
-				scale.y = value;
+				scale.y += value*inputMultiplier;
+				scale.y *= dampening;//depreciation
+				if (scale.y > binMaxHeight)
+					scale.y = binMaxHeight;
 				matrix[i][j].transform.localScale = scale;
 				matrix[i][j].transform.position = new Vector3(matrix[i][j].transform.position.x, matrix[i][j].transform.localScale.y/2, matrix[i][j].transform.position.z);
+
+				if (colorsEnabled)
+					hsbColor = new HSBColor(scale.y/binMaxHeight,1.0f,1.0f,1.0f);
+				matrix[i][j].gameObject.GetComponent<Renderer>().material.color = hsbColor.ToColor();
 				
 				scale = matrix[j][i].transform.localScale;
-				scale.y = value;
+				scale.y += value*inputMultiplier;
+				scale.y *= dampening;//depreciation
+				if (scale.y > binMaxHeight)
+					scale.y = binMaxHeight;
 				matrix[j][i].transform.localScale = scale;
 				matrix[j][i].transform.position = new Vector3(matrix[j][i].transform.position.x, matrix[j][i].transform.localScale.y/2, matrix[j][i].transform.position.z);
+
+				if (colorsEnabled)
+					hsbColor = new HSBColor(scale.y/binMaxHeight,1.0f,1.0f,1.0f);
+				matrix[j][i].gameObject.GetComponent<Renderer>().material.color = hsbColor.ToColor();
 				
 				scale = matrix[j][lastBinIndex-i].transform.localScale;
-				scale.y = value;
+				scale.y += value*inputMultiplier;
+				scale.y *= dampening;//depreciation
+				if (scale.y > binMaxHeight)
+					scale.y = binMaxHeight;
 				matrix[j][lastBinIndex-i].transform.localScale = scale;
 				matrix[j][lastBinIndex-i].transform.position = new Vector3(matrix[j][lastBinIndex-i].transform.position.x, matrix[j][lastBinIndex-i].transform.localScale.y/2, matrix[j][lastBinIndex-i].transform.position.z);
+				if (colorsEnabled)
+					hsbColor = new HSBColor(scale.y/binMaxHeight,1.0f,1.0f,1.0f);
+				matrix[j][lastBinIndex-i].gameObject.GetComponent<Renderer>().material.color = hsbColor.ToColor();
 				
 				scale = matrix[lastBinIndex-i][j].transform.localScale;
-				scale.y = value;
+				scale.y += value*inputMultiplier;
+				scale.y *= dampening;//depreciation
+				if (scale.y > binMaxHeight)
+					scale.y = binMaxHeight;
 				matrix[lastBinIndex-i][j].transform.localScale = scale;
 				matrix[lastBinIndex-i][j].transform.position = new Vector3(matrix[lastBinIndex-i][j].transform.position.x, matrix[lastBinIndex-i][j].transform.localScale.y/2, matrix[lastBinIndex-i][j].transform.position.z);
+
+				if (colorsEnabled)
+					hsbColor = new HSBColor(scale.y/binMaxHeight,1.0f,1.0f,1.0f);
+				matrix[lastBinIndex-i][j].gameObject.GetComponent<Renderer>().material.color = hsbColor.ToColor();
+
 			}
-			
-			
 			i++;
 		}
 	}
